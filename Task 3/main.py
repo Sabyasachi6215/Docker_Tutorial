a58eb5593b16749d0eee73c409f9fd5be558be9b
Sabyasachi <sabyarjit.ghosh@gmail.com>
2022-10-13 21:54:34 +0530

    The final commit for fast-api web app with docker image

--- Task 3/main.py
+++ Task 3/main.py
@@ -3,3 +3,6 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World" "I am creating docker Images for fastapi Application"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
