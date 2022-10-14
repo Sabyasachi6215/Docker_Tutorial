a6631d1368278d3c70a5ef243213c3a4c2d0ceeb
Sabyasachi <sabyarjit.ghosh@gmail.com>
2022-10-14 10:01:26 +0530

    This commit for Github Actions

---
+++ Task4/main.py
@@ -0,0 +1,14 @@
+#flask app for hello world
+from flask import Flask
+import pandas as pd
+import numpy as np
+
+app=Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def home():
+    return "Hello World- I am using Github Actions with this app"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
