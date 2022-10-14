648b70b2ce79c9da5820c74ad6087e6de6b059da
Sabyasachi <54863241+Sabyasachi6215@users.noreply.github.com>
2022-10-14 13:52:06 +0530

    Update main.py

--- Task4/main.py
+++ Task4/main.py
@@ -8,7 +8,7 @@ app=Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def home():
-    return "Hello World- I am using Github Actions with this app  Hi !!! Hello !!!"
+    return "Hello World- I am using Github Actions with this app in newbranch gitactions"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000)
